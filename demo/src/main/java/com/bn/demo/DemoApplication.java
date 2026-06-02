d8f112053a7a1efbf2d1f185c27da33d86b63811
vhpsgobbi <victorhugosgobbi@terra.com.br>
2026-06-01 22:10:56 -0300

    add exlplicacao demo aplication

--- demo/src/main/java/com/bn/demo/DemoApplication.java
+++ demo/src/main/java/com/bn/demo/DemoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// essa anotação faz um monte de coisa ao mesmo tempo, ela ativa o spring, configura tudo automaticamente e faz o scan de todos os componentes da aplicação
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		// essa linha ela liga a aplicação inteira, sem ela não roda nada
 	}
 
 }
